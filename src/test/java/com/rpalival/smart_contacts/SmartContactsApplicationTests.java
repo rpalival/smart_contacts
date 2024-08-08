ae9e1616273f0c68f568615560ddb8c5a4dc23df
Raj Palival <rpalival@stevens.edu>
2024-08-08 18:46:16 -0400

    first boilerplate commit from spring initializer

---
+++ src/test/java/com/rpalival/smart_contacts/SmartContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpalival.smart_contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
