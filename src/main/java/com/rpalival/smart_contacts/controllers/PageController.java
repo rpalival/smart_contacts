cda7ae922dcb18b35dc17472011e18eef3b6bf70
Raj Palival <rpalival@stevens.edu>
2024-08-11 13:55:44 -0400

    using thymeleaf fragments and understanding the basic flow and setting up tailwindcss

--- src/main/java/com/rpalival/smart_contacts/controllers/PageController.java
+++ src/main/java/com/rpalival/smart_contacts/controllers/PageController.java
@@ -10,11 +10,25 @@ public class PageController {
     @RequestMapping("/home")
     public String home(Model model) {
         System.out.println("Home page handler");
-
         // sending data to view
         model.addAttribute("name", "Substring Technologies");
         model.addAttribute("youtubeChannel", "Learn with harry");
         model.addAttribute("githubRepo", "https://github.com/rpalival/smart_contacts");
         return "home";
     }
+    // about routes
+
+    @RequestMapping("/about")
+    public String aboutPage() {
+        System.out.println("About page loading");
+        return "about";
+    }
+
+    @RequestMapping("/services")
+    public String servicesPage() {
+        System.out.println("Services page loading");
+        return "services";
+    }
+
+    // services
 }
