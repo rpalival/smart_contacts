afb9b97ecd8264673037e0a76cfe5d3f3f52172e
Raj Palival <rpalival@stevens.edu>
2024-08-11 21:26:42 -0400

    Create Responsive Navbar and login, SignUp buttons

--- src/main/java/com/rpalival/smart_contacts/controllers/PageController.java
+++ src/main/java/com/rpalival/smart_contacts/controllers/PageController.java
@@ -2,6 +2,7 @@ package com.rpalival.smart_contacts.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,18 +18,34 @@ public class PageController {
         return "home";
     }
     // about routes
-
     @RequestMapping("/about")
     public String aboutPage() {
         System.out.println("About page loading");
         return "about";
     }
 
+    // services
     @RequestMapping("/services")
     public String servicesPage() {
         System.out.println("Services page loading");
         return "services";
     }
+    // contact page
+    @GetMapping("/contact")
+    public String contact() {
+        System.out.println("Contact page loading");
+        return "contact";
+    }
+    @GetMapping("/login")
+    public String login() {
+        System.out.println("Contact page loading");
+        return "login";
+    }
+    @GetMapping("/register")
+    public String register() {
+        System.out.println("Contact page loading");
+        return "register";
+    }
 
-    // services
+
 }
