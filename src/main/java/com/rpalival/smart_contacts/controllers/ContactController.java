de4294517845ff3c407a7905b23f122ae3c0a01b
Raj Palival <rpalival@stevens.edu>
2024-08-21 01:22:45 -0400

    validate the contact form on server side

--- src/main/java/com/rpalival/smart_contacts/controllers/ContactController.java
+++ src/main/java/com/rpalival/smart_contacts/controllers/ContactController.java
@@ -4,12 +4,17 @@ import com.rpalival.smart_contacts.entities.Contact;
 import com.rpalival.smart_contacts.entities.User;
 import com.rpalival.smart_contacts.forms.ContactForm;
 import com.rpalival.smart_contacts.helpers.Helper;
+import com.rpalival.smart_contacts.helpers.Message;
+import com.rpalival.smart_contacts.helpers.MessageType;
 import com.rpalival.smart_contacts.services.ContactService;
 import com.rpalival.smart_contacts.services.UserService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,25 +40,23 @@ public class ContactController {
     }
 
     @RequestMapping(value="/add", method= RequestMethod.POST)
-    public String saveContact(@ModelAttribute ContactForm contactForm, Authentication authentication){
+    public String saveContact(@Valid @ModelAttribute ContactForm contactForm, BindingResult result, Authentication authentication, HttpSession session){
 
         // validate form
         // process the contact picture
         // set the contact picture url
         // set the message for confirmation
-        
+        if(result.hasErrors()){
+            Message errorMessage = Message.builder().
+                    content("Please correct the following errors").
+                    type(MessageType.red).
+                    build();
+            session.setAttribute("message", errorMessage);
+            return "user/add_contact";
+        }
+
         String username = Helper.getEmailOfLoggedInUser(authentication);
         User user = userService.getUserByEmail(username);
-        Contact contact = getContact(contactForm, user);
-
-
-        contactService.save(contact);
-        System.out.println(contactForm);
-
-        return "redirect:/user/contacts/add";
-    }
-
-    private static Contact getContact(ContactForm contactForm, User user) {
         Contact contact = new Contact();
 
         contact.setName(contactForm.getName());
@@ -65,6 +68,13 @@ public class ContactController {
         contact.setWebsiteLink(contactForm.getWebsiteLink());
         contact.setLinkedInLink(contactForm.getLinkedInLink());
         contact.setUser(user);
-        return contact;
+
+
+        contactService.save(contact);
+        System.out.println(contactForm);
+
+        Message message = Message.builder().content("You have successfully added a new contact").type(MessageType.green).build();
+        session.setAttribute("message", message);
+        return "redirect:/user/contacts/add";
     }
 }
