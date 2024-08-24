71f5e0acf638ddc6492a90191ffe413e4824130a
Raj Palival <rpalival@stevens.edu>
2024-08-23 20:01:31 -0400

    create pagination logic for contact page and search result

--- src/main/java/com/rpalival/smart_contacts/repositories/ContactRepo.java
+++ src/main/java/com/rpalival/smart_contacts/repositories/ContactRepo.java
@@ -2,6 +2,8 @@ package com.rpalival.smart_contacts.repositories;
 
 import com.rpalival.smart_contacts.entities.Contact;
 import com.rpalival.smart_contacts.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,8 +14,12 @@ import java.util.List;
 @Repository
 public interface ContactRepo extends JpaRepository<Contact, String> {
 
-    List<Contact> findByUser(User user);
+    Page<Contact> findByUser(User user, Pageable pageable);
 
     @Query("SELECT c FROM Contact c WHERE c.user.id = :userId")
     List<Contact> findByUserId(@Param("userId") String userId);
+    
+    Page<Contact> findByUserAndNameContaining (User user, String namekeyword, Pageable pageable);
+    Page<Contact> findByUserAndEmailContaining(User user, String emailkeyword, Pageable pageable);
+    Page<Contact> findByUserAndPhoneNumberContaining(User user, String phoneNumberkeyword, Pageable pageable);
 }
